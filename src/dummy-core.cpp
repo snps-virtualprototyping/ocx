4bdca6e3eedbb9b9f49bd436547f6289d3044f71
tobies <tobies@synopsys.com>
2019-11-14 14:20:25 +0100

    Added minimal dummycore implementation

--- src/dummy-core.cpp
+++ src/dummy-core.cpp
@@ -6,22 +6,198 @@
 *******************************************************************************/
 
 #include <iostream>
+#include <unistd.h>
 
 #include "ocx/ocx.h"
 
 namespace ocx {
 
+    class dummycore: public core
+    {
+    public:
+        dummycore():
+            core(), m_num_insn() {
+        }
+
+        virtual ~dummycore() {
+            return;
+        }
+
+        virtual const char* provider() override {
+            return "ocx::dummycore";
+        }
+
+        virtual const char* arch() override {
+            return "NONE";
+        }
+
+        virtual const char* arch_gdb() override {
+            return "NONE";
+        }
+
+        virtual const char* arch_family() override {
+            return "NONE";
+        }
+
+        virtual u64 page_size() override {
+            return 4096;
+        }
+
+        virtual void set_id(u64 procid, u64 coreid) override {
+            (void)procid;
+            (void)coreid;
+        }
+
+        virtual u64 step(u64 num_insn) override {
+            usleep(10);
+            m_num_insn += num_insn;
+            return num_insn;
+        }
+
+        virtual void stop() override {
+            return;
+        }
+
+        virtual u64 insn_count() override {
+            return m_num_insn;
+        }
+
+        virtual void reset() override {
+            return;
+        }
+
+        virtual void interrupt(u64 irq, bool set) override {
+            (void)irq;
+            (void)set;
+        }
+
+        virtual void notified(u64 eventid) override {
+            (void)eventid;
+        }
+
+        virtual u64 pc_regid() override {
+            return 0;
+        }
+
+        virtual u64 sp_regid() override {
+            return 0;
+        }
+
+        virtual u64 num_regs() override {
+            return 1;
+        }
+
+        virtual size_t reg_size(u64 regid) override {
+            (void)regid;
+            return 4;
+        }
+
+        virtual const char* reg_name(u64 regid) override {
+            (void)regid;
+            return "NONE";
+        }
+
+        virtual bool read_reg(u64 regid, void* buf) override {
+            (void)(regid);
+            (void)(buf);
+            return 0;
+        }
+
+        virtual bool write_reg(u64 regid, const void* buf) override {
+            (void)regid;
+            (void)buf;
+            return 0;
+        }
+
+        virtual bool add_breakpoint(u64 vaddr) override {
+            (void)vaddr;
+            return false;
+        }
+
+        virtual bool remove_breakpoint(u64 vaddr) override {
+            (void)vaddr;
+            return false;
+        }
+
+        virtual bool add_watchpoint(u64 vaddr, u64 sz, bool iswr) override {
+            (void)vaddr;
+            (void)sz;
+            (void)iswr;
+            return false;
+        }
+
+        virtual bool remove_watchpoint(u64 vaddr, u64 sz, bool iswr) override {
+            (void)vaddr;
+            (void)sz;
+            (void)iswr;
+            return false;
+        }
+
+        virtual bool trace_basic_blocks(bool on) override {
+            (void)on;
+            return false;
+        }
+
+        virtual bool virt_to_phys(u64 vaddr, u64& paddr) override {
+            (void)vaddr;
+            (void)paddr;
+            return false;
+        }
+
+        virtual void handle_syscall(int callno, void* arg) override {
+            (void)callno;
+            (void)arg;
+        }
+
+        virtual u64 disassemble(u64 addr, char* buf, size_t sz) override {
+            (void)addr;
+            (void)buf;
+            (void)sz;
+            return 0;
+        }
+
+        virtual void invalidate_page_ptrs() override {
+            return;
+        }
+
+        virtual void invalidate_page_ptr(u64 page_paddr) override {
+            (void)page_paddr;
+        }
+
+    private:
+        u64 m_num_insn;
+    };
+
     core* create_instance(u64 api_version, env& e, const char* variant) {
-        (void)e;
-        std::cerr << "This is just a dummy OpenCpuX stub, which does not "
-                  << "provide any functionality." << std::endl;
-        std::cerr << "Requested model was '" << variant << "', "
-                  << "API version " << api_version << std::endl;
-        return nullptr;
+        (void)(e);
+
+        static bool warned = false;
+        if (!warned) {
+            std::cerr << "This is just a dummy OpenCpuX stub, which does not "
+                      << "provide any functionality." << std::endl;
+            std::cerr << "Requested model was '" << variant << "', "
+                      << "API version " << api_version << std::endl;
+            warned = true;
+        }
+
+        if (api_version != OCX_API_VERSION)
+            return nullptr;
+
+        return new dummycore();
     }
 
-    void delete_instance(core* c) {
-        (void)c;
+    void delete_instance(core* cpu) {
+        if (cpu == nullptr)
+            return;
+
+        dummycore* dcpu = dynamic_cast<dummycore*>(cpu);
+        if (dcpu == nullptr) {
+            std::cerr << "attempt to delete foreign core " << cpu->provider()
+                      << std::endl;
+            abort();
+        }
+
+        delete dcpu;
     }
 
 }
